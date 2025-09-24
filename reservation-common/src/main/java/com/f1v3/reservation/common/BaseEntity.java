f10d5181af353bcfc4afba121f7e2031ffccc8a3
seungjo <f1v3@kakao.com>
2025-09-24 20:37:55 +0900

    feat: BaseEntity (생성일자, 수정일자)

---
+++ reservation-common/src/main/java/com/f1v3/reservation/common/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.f1v3.reservation.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.auditing.config.AuditingConfiguration;
+
+import java.time.LocalDateTime;
+
+/**
+ * 생성 및 수정 시간을 자동으로 관리하는 베이스 엔티티 클래스.
+ *
+ * @author Seungjo, Jeong
+ */
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingConfiguration.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+}
