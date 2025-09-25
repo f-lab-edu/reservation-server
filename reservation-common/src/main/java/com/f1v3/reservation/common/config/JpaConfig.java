43ac903005f3b3492e8033193ba0bca5c41603f2
seungjo <f1v3@kakao.com>
2025-09-25 14:37:24 +0900

    chore: JPA 설정

---
+++ reservation-common/src/main/java/com/f1v3/reservation/common/config/JpaConfig.java
@@ -0,0 +1,18 @@
+package com.f1v3.reservation.common.config;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * JPA 설정 클래스.
+ *
+ * @author Seungjo, Jeong
+ */
+@Configuration
+@EnableJpaAuditing
+@EntityScan(basePackages = {"com.f1v3.reservation.common"})
+@EnableJpaRepositories(basePackages = {"com.f1v3.reservation.common"})
+public class JpaConfig {
+}
