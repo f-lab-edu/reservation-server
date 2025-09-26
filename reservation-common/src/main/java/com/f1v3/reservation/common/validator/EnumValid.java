b0423a31587b1154139cb0b4aab8bb2b03ccd269
seungjo <f1v3@kakao.com>
2025-09-26 22:35:27 +0900

    feat: ENUM 유효성 검사 어노테이션 구현

---
+++ reservation-common/src/main/java/com/f1v3/reservation/common/validator/EnumValid.java
@@ -0,0 +1,26 @@
+package com.f1v3.reservation.common.validator;
+
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * ENUM 유효성 검사용 어노테이션.
+ *
+ * @author Seungjo, Jeong
+ */
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface EnumValid {
+
+    String message() default "Invalid value. This is not permitted.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    Class<? extends Enum<?>> enumClass();
+}
