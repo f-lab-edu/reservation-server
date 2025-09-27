4073a9bfbf98751abecbe5fd902968b28ef3e4f4
seungjo <f1v3@kakao.com>
2025-09-27 15:47:22 +0900

    refactor: 사용자용 약관 리스트 조회 응답 DTO 클래스 레코드로 변경

--- reservation-api/src/main/java/com/f1v3/reservation/api/term/dto/TermResponse.java
+++ reservation-api/src/main/java/com/f1v3/reservation/api/term/dto/TermResponse.java
@@ -3,34 +3,22 @@ package com.f1v3.reservation.api.term.dto;
 import com.f1v3.reservation.common.domain.term.dto.CurrentTermDto;
 import lombok.AccessLevel;
 import lombok.Builder;
-import lombok.Getter;
 
 /**
  * 약관 응답 DTO
  *
  * @author Seungjo, Jeong
  */
-@Getter
-public class TermResponse {
-    private final Long termId;
-    private final String termCode;
-    private final Integer version;
-    private final String title;
-    private final String type;
-    private final String content;
-    private final Integer displayOrder;
-
-    @Builder(access = AccessLevel.PRIVATE)
-    private TermResponse(Long termId, String termCode, Integer version, String title, String type, String content, Integer displayOrder) {
-        this.termId = termId;
-        this.termCode = termCode;
-        this.version = version;
-        this.title = title;
-        this.type = type;
-        this.content = content;
-        this.displayOrder = displayOrder;
-    }
-
+@Builder(access = AccessLevel.PRIVATE)
+public record TermResponse(
+        Long termId,
+        String termCode,
+        Integer version,
+        String title,
+        String type,
+        String content,
+        Integer displayOrder
+) {
     public static TermResponse from(CurrentTermDto term) {
         return TermResponse.builder()
                 .termId(term.getTermId())
