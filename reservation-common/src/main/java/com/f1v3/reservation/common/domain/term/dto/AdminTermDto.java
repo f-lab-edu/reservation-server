dad7a7e52c1ed055631922f7765ff3e1041e7017
seungjo <f1v3@kakao.com>
2025-09-27 13:14:46 +0900

    feat: Timestamp를 사용하여 날짜 필드 업데이트

--- reservation-common/src/main/java/com/f1v3/reservation/common/domain/term/dto/AdminTermDto.java
+++ reservation-common/src/main/java/com/f1v3/reservation/common/domain/term/dto/AdminTermDto.java
@@ -5,6 +5,7 @@ import com.f1v3.reservation.common.domain.term.enums.TermStatus;
 import com.f1v3.reservation.common.domain.term.enums.TermType;
 import lombok.Getter;
 
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 /**
@@ -35,23 +36,24 @@ public class AdminTermDto {
     private final LocalDateTime termVersionCreatedAt;
     private final LocalDateTime termVersionUpdatedAt;
 
-    public AdminTermDto(Long termId, TermCode termCode, String title, TermType type, Integer displayOrder, TermStatus status, LocalDateTime createdAt, LocalDateTime updatedAt,
-                        Long termVersionId, Integer version, Boolean isCurrent, String content, LocalDateTime effectiveDateTime, LocalDateTime expiryDateTime, LocalDateTime termVersionCreatedAt, LocalDateTime termVersionUpdatedAt) {
+
+    public AdminTermDto(Long termId, TermCode termCode, String title, TermType type, Integer displayOrder, TermStatus status, Timestamp createdAt, Timestamp updatedAt,
+                        Long termVersionId, Integer version, Boolean isCurrent, String content, LocalDateTime effectiveDateTime, LocalDateTime expiryDateTime, Timestamp termVersionCreatedAt, Timestamp termVersionUpdatedAt) {
         this.termId = termId;
         this.termCode = termCode.name();
         this.title = title;
         this.type = type.name();
         this.displayOrder = displayOrder;
         this.status = status.name();
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
+        this.createdAt = createdAt != null ? createdAt.toLocalDateTime() : null;
+        this.updatedAt = updatedAt != null ? updatedAt.toLocalDateTime() : null;
         this.termVersionId = termVersionId;
         this.version = version;
         this.isCurrent = isCurrent;
         this.content = content;
         this.effectiveDateTime = effectiveDateTime;
         this.expiryDateTime = expiryDateTime;
-        this.termVersionCreatedAt = termVersionCreatedAt;
-        this.termVersionUpdatedAt = termVersionUpdatedAt;
+        this.termVersionCreatedAt = termVersionCreatedAt != null ? termVersionCreatedAt.toLocalDateTime() : null;
+        this.termVersionUpdatedAt = termVersionUpdatedAt != null ? termVersionUpdatedAt.toLocalDateTime() : null;
     }
 }
