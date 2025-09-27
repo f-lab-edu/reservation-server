cd4a53ae560cb1846d2351317c5ec8049caf3aab
seungjo <f1v3@kakao.com>
2025-09-27 15:47:38 +0900

    feat: 약관 조회 메서드 Transactional 어노테이션 추가

--- reservation-api/src/main/java/com/f1v3/reservation/api/term/TermService.java
+++ reservation-api/src/main/java/com/f1v3/reservation/api/term/TermService.java
@@ -5,6 +5,7 @@ import com.f1v3.reservation.common.domain.term.dto.CurrentTermDto;
 import com.f1v3.reservation.common.domain.term.repository.TermRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -22,6 +23,7 @@ public class TermService {
     /**
      * 활성화된 약관들의 최신 버전 조회 (회원가입용)
      */
+    @Transactional(readOnly = true)
     public List<TermResponse> getActiveTerms() {
         List<CurrentTermDto> activeTermsWithVersion = termRepository.getActiveTermsWithVersion();
 
