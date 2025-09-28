3cb0fc1584d7ee5a91a3cbdf3513682e310a9b28
seungjo <f1v3@kakao.com>
2025-09-28 13:37:24 +0900

    feat: 최신 핸드폰 인증 조회 메서드 추가 (Querydsl)

--- reservation-common/src/main/java/com/f1v3/reservation/common/domain/phoneverification/repository/PhoneVerificationRepository.java
+++ reservation-common/src/main/java/com/f1v3/reservation/common/domain/phoneverification/repository/PhoneVerificationRepository.java
@@ -4,9 +4,10 @@ import com.f1v3.reservation.common.domain.phoneverification.PhoneVerification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
- * 휴대폰 인증 엔티티의 JPA Repository 인터페이스입니다.
+ * 휴대폰 인증 엔티티의 JPA Repository 인터페이스
  *
  * @author Seungjo, Jeong
  */
-public interface PhoneVerificationRepository extends JpaRepository<PhoneVerification, Long> {
+public interface PhoneVerificationRepository extends JpaRepository<PhoneVerification, Long>,
+        PhoneVerificationRepositoryCustom {
 }
---
+++ reservation-common/src/main/java/com/f1v3/reservation/common/domain/phoneverification/repository/PhoneVerificationRepositoryCustom.java
@@ -0,0 +1,17 @@
+package com.f1v3.reservation.common.domain.phoneverification.repository;
+
+import com.f1v3.reservation.common.domain.phoneverification.PhoneVerification;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import java.util.Optional;
+
+/**
+ * 휴대폰 인증 커스텀 리포지토리 인터페이스
+ *
+ * @author Seungjo, Jeong
+ */
+@NoRepositoryBean
+public interface PhoneVerificationRepositoryCustom {
+
+    Optional<PhoneVerification> findLatestByPhoneNumber(String phoneNumber);
+}
