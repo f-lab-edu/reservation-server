471a684dc4c4f58bd8fb8d7efded741e865d2abf
seungjo <f1v3@kakao.com>
2025-10-02 14:26:29 +0900

    feat: phoneNumber 기반 엔티티 조회 메서드 추가

--- reservation-common/src/main/java/com/f1v3/reservation/common/domain/phoneverification/repository/PhoneVerificationRepository.java
+++ reservation-common/src/main/java/com/f1v3/reservation/common/domain/phoneverification/repository/PhoneVerificationRepository.java
@@ -3,11 +3,14 @@ package com.f1v3.reservation.common.domain.phoneverification.repository;
 import com.f1v3.reservation.common.domain.phoneverification.PhoneVerification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 /**
  * 휴대폰 인증 엔티티의 JPA Repository 인터페이스
  *
  * @author Seungjo, Jeong
  */
-public interface PhoneVerificationRepository extends JpaRepository<PhoneVerification, Long>,
-        PhoneVerificationRepositoryCustom {
+public interface PhoneVerificationRepository extends JpaRepository<PhoneVerification, Long> {
+
+    Optional<PhoneVerification> findByPhoneNumber(String phoneNumber);
 }
