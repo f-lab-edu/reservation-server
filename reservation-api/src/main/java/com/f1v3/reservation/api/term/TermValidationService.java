c47252e5a982c36d94e76a40bbb30a5add2c64ef
seungjo <f1v3@kakao.com>
2025-10-02 14:29:28 +0900

    fix: 약관 엔티티 통합에 따른 검증 메서드 로직 변경

--- reservation-api/src/main/java/com/f1v3/reservation/api/term/TermValidationService.java
+++ reservation-api/src/main/java/com/f1v3/reservation/api/term/TermValidationService.java
@@ -2,11 +2,9 @@ package com.f1v3.reservation.api.term;
 
 import com.f1v3.reservation.api.term.dto.TermResponse;
 import com.f1v3.reservation.api.user.dto.SignupUserRequest;
-import com.f1v3.reservation.common.domain.term.enums.TermType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -22,32 +20,19 @@ public class TermValidationService {
     private final TermService termService;
 
     public void validateRequiredTermsAgreed(Set<SignupUserRequest.SignupTermRequest> termRequests) {
-        Set<Long> requiredTermIds = getRequiredTermIds();
-        Set<Long> agreedTermIds = getAgreedTermIds(termRequests);
-
-        Set<Long> missingRequiredTerms = getMissingRequiredTerms(requiredTermIds, agreedTermIds);
-
-        if (!missingRequiredTerms.isEmpty()) {
-            throw new IllegalArgumentException("필수 약관에 모두 동의해야 합니다. 누락된 약관 id=" + missingRequiredTerms);
-        }
-    }
-
-    private Set<Long> getMissingRequiredTerms(Set<Long> requiredTermIds, Set<Long> agreedTermIds) {
-        Set<Long> missingAgreements = new HashSet<>(requiredTermIds);
-        missingAgreements.removeAll(agreedTermIds);
-        return missingAgreements;
-    }
-
-    private Set<Long> getRequiredTermIds() {
-        return termService.getActiveTerms().stream()
-                .filter(term -> TermType.REQUIRED.name().equals(term.getType()))
-                .map(TermResponse::getTermId)
+        Set<Long> requiredTermIds = termService.getActiveTerms().stream()
+                .filter(TermResponse::isRequired)
+                .map(TermResponse::termId)
                 .collect(Collectors.toSet());
-    }
 
-    private Set<Long> getAgreedTermIds(Set<SignupUserRequest.SignupTermRequest> termRequests) {
-        return termRequests.stream()
+        Set<Long> agreedTermIds = termRequests.stream()
                 .map(SignupUserRequest.SignupTermRequest::termId)
                 .collect(Collectors.toSet());
+
+        requiredTermIds.removeAll(agreedTermIds);
+
+        if (!requiredTermIds.isEmpty()) {
+            throw new IllegalArgumentException("필수 약관에 모두 동의해야 합니다. 누락된 약관 id=" + requiredTermIds);
+        }
     }
 }
