5c6d6addb29eabc75f71949d56c65760355df22a
seungjo <f1v3@kakao.com>
2025-11-28 00:44:21 +0900

    feat: Redisson 클라이언트 설정 추가

---
+++ reservation-common/src/main/java/com/f1v3/reservation/common/config/RedissonConfig.java
@@ -0,0 +1,32 @@
+package com.f1v3.reservation.common.config;
+
+import lombok.RequiredArgsConstructor;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Redisson Client 설정 클래스
+ *
+ * @author Seungjo, Jeong
+ */
+@Configuration
+@RequiredArgsConstructor
+public class RedissonConfig {
+
+    private static final String REDISSON_HOST_PREFIX = "redis://";
+    private final RedisProperties redisProperties;
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress(REDISSON_HOST_PREFIX + redisProperties.getHost() + ":" + redisProperties.getPort())
+                .setPassword(redisProperties.getPassword());
+
+        return Redisson.create(config);
+    }
+}
