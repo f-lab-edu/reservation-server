072dafce8be384f0dfa6a5adc15aefa07f20b272
seungjo <f1v3@kakao.com>
2025-12-21 16:04:21 +0900

    fix: 가계약 생성 응답의 holdKey를 holdId로 변경

--- reservation-api/src/main/java/com/f1v3/reservation/api/reservation/dto/ReservationHoldResponse.java
+++ reservation-api/src/main/java/com/f1v3/reservation/api/reservation/dto/ReservationHoldResponse.java
@@ -3,12 +3,12 @@ package com.f1v3.reservation.api.reservation.dto;
 import java.time.LocalDateTime;
 
 /**
- * 임시 예약 생성 응답
+ * 가계약 생성 응답
  *
  * @author Seungjo, Jeong
  */
 public record ReservationHoldResponse(
-        String holdKey,
+        String holdId,
         LocalDateTime expiredAt
 ) {
 }
